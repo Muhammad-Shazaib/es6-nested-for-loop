5324230669081e04f438348fbcec8dbd77142330
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-05-30 20:42:02 +0500

    first commit

---
+++ app.mjs
@@ -0,0 +1,15 @@
+// ek array ka ander dusra array aa jay to usay 2d(2 dimention array) array kehte hain
+// nested array bhe khaty hain
+
+let matrix1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for (let i = 0; i < matrix1.length; i++) {
+
+    console.log('some text', matrix1[i]);
+}
+
+
